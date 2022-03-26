623cc1339b879c5d08d81bb95ea8e6d066f3f297
Vainius <vmykolaitis1@gmail.com>
2022-03-26 16:49:29 +0200

    Added /get-average-holdings route

--- routes/holdersRoutes.js
+++ routes/holdersRoutes.js
@@ -1,11 +1,24 @@
+const HolderWallets = require('../src/services/HolderWallets');
+
 const getHoldersCount = require('../src/commands/getHoldersCount');
 const updateWalletsFromPreviousBlock = require('../src/commands/updateWalletsFromPreviousBlock');
+
+const holderWallets = new HolderWallets();
 
 module.exports = (app) => {
   app.get('/get-holders', async (req, res) => {
     try {
       const holders = await getHoldersCount();
       res.json({ holders });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  app.get('/get-average-holdings', async (req, res) => {
+    try {
+      const average = await holderWallets.getAverageHoldings();
+      res.json({ average });
     } catch (err) {
       console.log(err);
     }
