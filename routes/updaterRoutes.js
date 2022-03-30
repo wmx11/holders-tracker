43367d44c30e53279ba6cd40b3dc0f584db0776a
Vainius <vmykolaitis1@gmail.com>
2022-03-30 23:29:40 +0300

    Added a function call for the partial-wallets-update endpoint

--- routes/updaterRoutes.js
+++ routes/updaterRoutes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   app.get('/partial-wallets-update', async (req, res) => {
     try {
       res.send('Updating wallets');
-      require('../src/commands/partialWalletsUpdate');
+      return require('../src/commands/partialWalletsUpdate')();
     } catch (err) {
       console.log(err);
     }
