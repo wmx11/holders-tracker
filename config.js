d37a27551824ef92d04d038ae8cafd53a5c81eb1
Vainius <vmykolaitis1@gmail.com>
2022-05-16 15:40:18 +0300

    Changed contract address and block

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   walletAddress: '0x6c957d7030fbac6e070c84b4370e3c8cb6e99cd7',
-  contractAddress: '0xBA96731324dE188ebC1eD87ca74544dDEbC07D7f',
-  contractCreationStartBlock: 12692759,
+  contractAddress: '0x4e3cABD3AD77420FF9031d19899594041C420aeE',
+  contractCreationStartBlock: 17166684,
   rpcEndpoint: 'https://bsc-dataseed1.binance.org',
   blockChunks: 2000,
   requestTimeout: 20,
