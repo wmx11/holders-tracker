f2136f8e4a66708ad86f52badc39d23f7fb16ba1
Vainius <vmykolaitis1@gmail.com>
2022-06-15 12:07:14 +0300

    Added holders notes and isProjectWallet

---
+++ prisma/migrations/20220615083849_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Holders` ADD COLUMN `note` VARCHAR(191) NULL;
---
+++ prisma/migrations/20220615084100_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Holders` ADD COLUMN `isProjectWallet` BOOLEAN NOT NULL DEFAULT false;
